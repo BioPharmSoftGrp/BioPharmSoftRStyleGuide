0ad8dcc8a380a907d03c557a58ffa21e209d99cf
J. Kyle Wathen, PhD <kyle.wathen@gilead.com>
2020-12-02 15:38:19 -0600

    Updating the GitHub branches

---
+++ RExamples.R
@@ -0,0 +1,27 @@
+# Bad Example - Ending a solid line of 4 - or = result in an (Untitled) in the outline
+
+#-------------------------------------------------------
+# Add 2 - This function adds 2 to x
+#-------------------------------------------------------
+
+Add2 <- function(x){
+    return(x + 2)
+}
+
+#-------------------------------------------------------
+# Subtract 2 - This function subtracts 2 from x
+#-------------------------------------------------------
+
+Subtract2 <- function(x){
+    return(x - 2)
+}
+
+#-------------------------------------------------------
+#  Divide variable by 2
+#-------------------------------------------------------
+
+Divide2 <- function(x){
+    return(x/2)
+}
+
+x-y<-2
